d3793ff0e7b13fb3c6b98a765902adf4423bae63
oesteban <code@oscaresteban.es>
2016-08-29 17:24:35 -0700

    ugly hack trying to fix up RTD build

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # @Author: oesteban
 # @Date:   2015-11-19 16:44:27
 # @Last Modified by:   oesteban
-# @Last Modified time: 2016-08-29 16:37:01
+# @Last Modified time: 2016-08-29 17:20:02
 """ MRIQC setup script """
 import os
 import sys
@@ -71,8 +71,8 @@ def main():
 
 if __name__ == '__main__':
     from subprocess import call
-    call(['/home/docs/checkouts/readthedocs.org/user_builds/mriqc/envs/latest/bin/pip',
-          'install', '-e', 'git+https://github.com/nipy/nipype#egg=nipype'])
+    call(['pip', 'install', '-e', 'git+https://github.com/nipy/nipype#egg=nipype'])
+    call(['pip', 'install', 'niworkflows'])
     LOCAL_PATH = os.path.dirname(os.path.abspath(sys.argv[0]))
     os.chdir(LOCAL_PATH)
     sys.path.insert(0, LOCAL_PATH)
